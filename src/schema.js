65a14b3a6abd28d03eca4b6b6a0a78798493a4a2
tangshuang <mail@tangshuang.net>
2020-01-02 22:58:21 +0800

    update

--- src/schema.js
+++ src/schema.js
@@ -6,7 +6,7 @@ import {
   isEqual,
   isInstanceOf,
   isEmpty,
-} from 'ts-fns'
+} from 'ts-fns/es/is.js'
 import {
   each,
   map,
@@ -19,60 +19,53 @@ import TyError from './ty-error.js'
 import Ty from './ty.js'
 import { ifexist } from './rules.js'
 
+/**
+ * new Schema({
+ *   property: {
+ *     default: '', // required
+ *
+ *     // optional, computed property, will compute at each time digest end
+ *     // when it is a compute property, it is not able to use set to update value
+ *     compute: function() {
+ *       const a = this.get('a')
+ *       const b = this.get('b')
+ *       return a + '' + b
+ *     },
+ *
+ *     type: String, // required, notice: `default` and result of `compute` should match type
+ *     rule: ifexist, // optional, which rule to use, only `ifexist` `instance` and `equal` allowed
+ *
+ *     validators: [ // optional
+ *       {
+ *         determine: (value) => Boolean, // whether to run this validator, return true to run, false to forbid
+ *         validate: (value) => Boolean, // whether to pass the validate, return true to pass, false to not pass and throw error
+ *         message: '', // the message of error which throw when validate not pass
+ *       },
+ *     ],
+ *
+ *     prepare: (data) => !!data.on_market, // optional, function, used by `rebuild`, `data` is the parameter of `rebuild`
+ *
+ *     drop: (value, key, data) => Boolean, // optional, function, whether to not use this property when invoke `jsondata` and `formdata`
+ *     map: (value, key, data) => newValue, // optional, function, to override the property value when using `jsondata` and `formdata`, not work when `drop` is false
+ *     flat: (value, key, data) => ({ newProp: newValue }), // optional, function, to assign this result to output data, don't forget to set `drop` to be true if you want to drop original data
+ *
+ *     getter: (value) => newValue, // optional, function, format this property value when get
+ *     setter: (value) => value, // optional, function, format this property value when set
+ *
+ *     required: () => Boolean, // optional, function or boolean, use schema.required(field) to check, will be invoked by validate
+ *     disabled: () => Boolean, // optional, function or boolean, use schema.disabled(field) to check, will disable set/validate, preload before drop in formulate
+ *     readonly: () => Boolean, // optional, function or boolean, use schema.readonly(field) to check, will disable set
+ *     // the difference between `disabled` and `readonly`:
+ *     // disabled is to disable this property, so that it should not be used(shown) in your application, could not be changed, validate will not work, and will be dropped when formulate,
+ *     // readonly means the property can only be read/validate/formulate, but could not be changed.
+ *
+ *     message: '', // the message when type checking or required or aother checking fail
+ *     catch: (error) => {}, // when an error occurs caused by this property, what to do with the error
+ *   },
+ * })
+ */
 export class Schema {
-
-  /**
-   * schema definition, should be an object:
-   * {
-   *   property: {
-   *     default: '', // required
-   *
-   *     // optional, computed property, will compute at each time digest end
-   *     // when it is a compute property, it is not able to use set to update value
-   *     compute: function() {
-   *       const a = this.get('a')
-   *       const b = this.get('b')
-   *       return a + '' + b
-   *     },
-   *
-   *     type: String, // required, notice: `default` and result of `compute` should match type
-   *     rule: ifexist, // optional, which rule to use, only `ifexist` `instance` and `equal` allowed
-   *     message: '', // the message when type checking and required checking fail
-   *     validators: [ // optional
-   *       {
-   *         determine: (value) => Boolean, // whether to run this validator, return true to run, false to forbid
-   *         validate: (value) => Boolean, // whether to pass the validate, return true to pass, false to not pass and throw error
-   *         message: '', // the message of error which throw when validate not pass
-   *       },
-   *     ],
-   *
-   *     prepare: (data) => !!data.on_market, // optional, function, used by `rebuild`, `data` is the parameter of `rebuild`
-   *
-   *     drop: (value, key, data) => Boolean, // optional, function, whether to not use this property when invoke `jsondata` and `formdata`
-   *     map: (value, key, data) => newValue, // optional, function, to override the property value when using `jsondata` and `formdata`, not work when `drop` is false
-   *     flat: (value, key, data) => ({ newProp: newValue }), // optional, function, to assign this result to output data, don't forget to set `drop` to be true if you want to drop original data
-   *
-   *     getter: (value) => newValue, // optional, function, format this property value when get
-   *     setter: (value) => value, // optional, function, format this property value when set
-   *
-   *     required: () => Boolean, // optional, function or boolean, use schema.required(field) to check, will be invoked by validate
-   *     disabled: () => Boolean, // optional, function or boolean, use schema.disabled(field) to check, will disable set/validate, preload before drop in formulate
-   *     readonly: () => Boolean, // optional, function or boolean, use schema.readonly(field) to check, will disable set
-   *     // the difference between `disabled` and `readonly`:
-   *     // disabled is to disable this property, so that it should not be used(shown) in your application, could not be changed, validate will not work, and will be dropped when formulate,
-   *     // readonly means the property can only be read/validate/formulate, but could not be changed.
-   *
-   *     catch: (error) => {}, // when an error occurs caused by this property, what to do with the error
-   *   },
-   * }
-   */
   constructor(definition) {
-    each(definition, (def, key) => {
-      if (!isObject(def)) {
-        throw new Error(`[Schema]: definition '${key}' should be an object.`)
-      }
-    })
-
     this.definition = definition
   }
 
@@ -139,7 +132,12 @@ export class Schema {
       return value
     }
 
-    const { getter } = def
+    const { getter, compute } = def
+    if (isFunction(compute)) {
+      const next = compute.call(context)
+      return next
+    }
+
     const next = isFunction(getter) ? getter.call(context, value) : value
 
     return next
