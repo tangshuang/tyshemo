c08f5bbd28ab0ac5b6905532f5de494c35ad14ea
tangshuang <mail@tangshuang.net>
2020-08-10 14:37:11 +0800

    update Validator

--- src/validator.js
+++ src/validator.js
@@ -1,86 +1,4 @@
-import { isEmpty, isNumber, isNumeric, isString, createSafeExp } from 'ts-fns'
-
-function required() {
-  return value => !isEmpty(value)
-}
-
-function maxLen(len) {
-  return value => isString(value) && value.length <= len
-}
-
-function minLen(len) {
-  return value => isString(value) && value.length >= len
-}
-
-function numeral(integer, decimal) {
-  return (value) => {
-    const max = Math.pow(10, integer)
-    const fix = Math.pow(10, decimal)
-
-    if (!isNumber(value) && !isNumeric(value)) {
-      return false
-    }
-
-    const num = +value
-    if (num >= max) {
-      return false
-    }
-    else if (num * fix % 1 !== 0) {
-      return false
-    }
-    else {
-      return true
-    }
-  }
-}
-
-function max(num) {
-  return value => (isNumber(value) || isNumeric(value)) && +value <= num
-}
-
-function min(num) {
-  return value => (isNumber(value) || isNumeric(value)) && +value >= num
-}
-
-function email() {
-  return value => isString(value) && /^[A-Za-z0-9]+[A-Za-z0-9\._]*[A-Za-z0-9]+@[A-Za-z0-9]+[A-Za-z0-9\.\-]*[A-Za-z0-9]+\.[A-Za-z]{2,8}$/.test(value)
-}
-
-function url() {
-  return value => isString(value) && /(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/.test(value)
-}
-
-function date(sep = '-') {
-  return value => isString(value) && new RegExp(`^[1-2][0-9]{3}${createSafeExp(sep)}[0-1][0-9]${createSafeExp(sep)}[0-3][0-9]`).test(value)
-}
-
-function match(reg) {
-  return value => typeof value === 'string' && reg.test(value)
-}
-
-function merge(...validators) {
-	return function(value) {
-		for (const i = 0, len = validators.length; i < len; i ++) {
-			const validate = validators[i]
-			if (!validate.call(this, value)) {
-				return false
-			}
-		}
-		return true
-	}
-}
-
-function enume(...validators) {
-	return function(value) {
-		for (const i = 0, len = validators.length; i < len; i ++) {
-			let validate = validators[i]
-			if (validate.call(this, value)) {
-				return true
-			}
-		}
-		return false
-	}
-}
+import { isEmpty, isNumber, isNumeric, isString } from 'ts-fns'
 
 export class Validator {
   constructor(attrs = {}) {
@@ -88,6 +6,7 @@ export class Validator {
     const finalAttrs = { ...Constructor, ...attrs }
     Object.assign(this, finalAttrs)
   }
+
   extend(attrs = {}) {
     const Constructor = getConstructorOf(this)
     const finalAttrs = { ...this, ...attrs }
@@ -109,8 +28,137 @@ export class Validator {
   static match = match
   static maxLen = maxLen
   static minLen = minLen
-
   static merge = merge
   static enume = enume
 }
 export default Validator
+
+function required(message) {
+  return new Validator({
+    validate: value => !isEmpty(value),
+    message,
+    break: true,
+  })
+}
+
+function maxLen(len, message) {
+  return new Validator({
+    validate: value => isString(value) && value.length <= len,
+    message,
+    break: true,
+  })
+}
+
+function minLen(len, message) {
+  return new Validator({
+    validate: value => isString(value) && value.length >= len,
+    message,
+    break: true,
+  })
+}
+
+function numeral(integer, decimal, message) {
+  return new Validator({
+    validate: (value) => {
+      const max = Math.pow(10, integer)
+      const fix = Math.pow(10, decimal)
+
+      if (!isNumber(value) && !isNumeric(value)) {
+        return false
+      }
+
+      const num = +value
+      if (num >= max) {
+        return false
+      }
+      else if (num * fix % 1 !== 0) {
+        return false
+      }
+      else {
+        return true
+      }
+    },
+    message,
+    break: true,
+  })
+}
+
+function max(num, message) {
+  return new Validator({
+    validate: value => (isNumber(value) || isNumeric(value)) && +value <= num,
+    message,
+    break: true,
+  })
+}
+
+function min(num, message) {
+  return new Validator({
+    validate: value => (isNumber(value) || isNumeric(value)) && +value >= num,
+    message,
+    break: true,
+  })
+}
+
+function email(message) {
+  return new Validator({
+    validate: value => isString(value) && /^[A-Za-z0-9]+[A-Za-z0-9\._]*[A-Za-z0-9]+@[A-Za-z0-9]+[A-Za-z0-9\.\-]*[A-Za-z0-9]+\.[A-Za-z]{2,8}$/.test(value),
+    message,
+    break: true,
+  })
+}
+
+function url(message) {
+  return new Validator({
+    validate: value => isString(value) && /(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/.test(value),
+    message,
+    break: true,
+  })
+}
+
+function date(message) {
+  return new Validator({
+    validate: value => isString(value) && /^[1-2][0-9]{3}\-[0-1][0-9]\-[0-3][0-9]/.test(value),
+    message,
+    break: true,
+  })
+}
+
+function match(reg, message) {
+  return new Validator({
+    validate: value => typeof value === 'string' && reg.test(value),
+    message,
+    break: true,
+  })
+}
+
+function merge(validators, message) {
+  return new Validator({
+    validate(value) {
+      for (const i = 0, len = validators.length; i < len; i ++) {
+        const validate = validators[i]
+        if (!validate.call(this, value)) {
+          return false
+        }
+      }
+      return true
+    },
+    message,
+    break: true,
+  })
+}
+
+function enume(validators, message) {
+  return new Validator({
+    validate(value) {
+      for (const i = 0, len = validators.length; i < len; i ++) {
+        let validate = validators[i]
+        if (validate.call(this, value)) {
+          return true
+        }
+      }
+      return false
+    },
+    message,
+    break: true,
+  })
+}
