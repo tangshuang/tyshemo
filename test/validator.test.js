7527aa8f9dfd0f46f90ac9b663939dc3565c9b58
tangshuang <mail@tangshuang.net>
2021-08-19 16:40:35 +0800

    test: add unit test for validator

--- test/validator.test.js
+++ test/validator.test.js
@@ -22,4 +22,26 @@ describe('Validator', () => {
     expect(errors.length).toBe(1)
     expect(errors[0].message).toBe('Some is required')
   })
+
+  test('default return false in validate function', () => {
+    const Some = new Meta({
+      default: 'aaa',
+      validators: [
+        {
+          validate: () => false,
+          message: 'default return validate',
+        },
+      ],
+    })
+
+    class SomeModel extends Model {
+      static some = Some
+    }
+
+    const some = new SomeModel()
+    const errors = some.validate()
+
+    expect(errors.length).toBe(1)
+    expect(errors.message).toBe('default return validate')
+  })
 })
