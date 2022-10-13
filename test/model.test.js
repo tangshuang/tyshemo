2b6db6dd0f3779737e3f008e7dd3226b0a3c6d7a
tangshuang <mail@tangshuang.net>
2022-10-13 12:47:15 +0800

    fix: unit test

--- test/model.test.js
+++ test/model.test.js
@@ -461,7 +461,7 @@ describe('Model', () => {
     expect(some.child.name).toBe('lily')
 
     // use static method of Model
-    const editor = new Some.Edit(some)
+    const editor = new (Some.Edit())(some)
 
     editor.name = 'tomi'
     expect(some.name).toBe('tomy')
